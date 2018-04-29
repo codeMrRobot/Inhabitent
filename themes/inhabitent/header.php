348dffedd94da0f8205095bec464418cd7cbe9ed
Tony <diablotony01@hotmail.com>
2018-04-29 04:23:36 -0700

    fixed navigation and added logo

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -13,7 +13,6 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		
 
 	<?php wp_head(); ?>
 	</head>
@@ -21,18 +20,29 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
+			<header id="masthead" class="site-header reverse-header" role="banner">
+				<div class="header-container">		
+					<div class="site-branding">
+							<h1 class="site-title screen-reader-text">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?>
+								<div class="header-logo"></div>
+								</a>
+							</h1>
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+									<img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="inhabitent logo" />
+								</a>
+						
+							</div><!-- .site-branding -->
+						<div>
+							<nav id="site-navigation" class="main-navigation" role="navigation">
+								<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+								<div>
+								<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="container">
-				<div class="site-branding"></div>
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+								</div>
+								<div><?php get_search_form();?></div>
+							</nav><!-- #site-navigation -->
+					</div>
+				</div>		
 			</header><!-- #masthead -->
 
-			<div id="content" class="site-content">
