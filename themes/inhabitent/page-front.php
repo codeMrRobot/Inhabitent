88a2e62a02561da01d5f30b06783f88207f792fa
Tony <diablotony01@hotmail.com>
2018-05-14 01:03:14 -0700

    added is home && is front page

--- themes/inhabitent/page-front.php
+++ themes/inhabitent/page-front.php
@@ -11,11 +11,14 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <?php if ( is_home() && ! is_front_page() ) : ?>
     
 
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title();  ?></h1>
-				</header>
+                </header>
+
+            <?php endif; ?>
 	    <div class="hero-banner">
 				<img src=<?php echo get_template_directory_uri() . "/images/logos/inhabitent-logo-full.svg" ?> >
       </div>	
