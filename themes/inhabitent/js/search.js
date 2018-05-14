3d820df05d2aa1dc0f4b6b4620558a6272f225dd
Tony <diablotony01@hotmail.com>
2018-05-14 14:30:05 -0700

    added search js function

---
+++ themes/inhabitent/js/search.js
@@ -0,0 +1,16 @@
+(function ($){
+
+
+  $('.search-toggle').on('click', function() {
+    $('.search-field').animate( { width: "200px" }, 500 ).focus();
+  });
+
+  $('.search-field').on('blur', function() {
+      $('.search-field').animate( { width: "0px", border:"none" }, 500 );
+  });
+
+  $('.search-field').animate( { width: 0,border: 'none', }, 0 );
+    
+  
+
+})(jQuery);
