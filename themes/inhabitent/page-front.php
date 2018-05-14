a7622095acd6d8b807736a2fe5885c3865773902
Tony <diablotony01@hotmail.com>
2018-05-14 01:10:55 -0700

    added shop type product stuff tax

--- themes/inhabitent/page-front.php
+++ themes/inhabitent/page-front.php
@@ -19,9 +19,31 @@ get_header(); ?>
                 </header>
 
             <?php endif; ?>
+
 	    <div class="hero-banner">
 				<img src=<?php echo get_template_directory_uri() . "/images/logos/inhabitent-logo-full.svg" ?> >
-      </div>	
+      </div>
+      
+      <section class="product-info container max-contain">
+				<h2>shop stuff</h2>
+				<?php
+					$terms = get_terms( array(
+							'taxonomy' => 'product-type',
+							'hide_empty' => 0,
+					) );
+					if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+				?>
+					<div class="product-type-blocks">
+						<?php foreach ( $terms as $term ) : ?>
+								<div class="product-type-block-wrapper">
+									<img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+									<p><?php echo $term->description; ?></p>
+									<p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+								</div>
+						<?php endforeach; ?>
+					</div>			
+				<?php endif; ?>
+      </section>
       
 
 
