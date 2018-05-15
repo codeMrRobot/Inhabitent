3afd00c994ce235445e2e67e0a94701bc0d7272e
Tony <diablotony01@hotmail.com>
2018-05-15 13:42:16 -0700

    edit jquery in function php

--- themes/inhabitent/inc/enqueue.php
+++ themes/inhabitent/inc/enqueue.php
@@ -6,6 +6,9 @@
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 
+
+
+
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
