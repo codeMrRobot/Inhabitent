6b20fc5406d9060fbf4af021df75ce14023a284a
Tony <diablotony01@hotmail.com>
2018-05-14 22:30:33 -0700

    deleted page template about and moved it out

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -1,23 +1,29 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: About Page
  *
  * @package RED_Starter_Theme
  */
-
 get_header(); ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header custom-hero">
+          <div class="container">
+            <?php the_title ( '<h1 class="entry-title">', '</h1>' ); ?>
+          </div>
+        </header
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+        <div class="container">
+          <div class="entry-content">
+            <h2>Our Story</h2>
+            <?php echo CFS()->get( 'about_our_story' );?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+            <h2>Our Team</h2>
+            <?php echo CFS()->get( 'about_us_team' );?>
+          </div>     
+        </div>
+      </article>
+    </main>
+  </div>
+<?php get_footer(); ?>      
