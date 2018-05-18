f119c987bec9f4d14dd48a5778716fee91db4d5a
Tony <diablotony01@hotmail.com>
2018-05-17 18:08:19 -0700

    edit functions php

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -86,8 +86,8 @@ function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	// wp_enqueue_style( 'tent-style', get_stylesheet_uri() );
 	
-	// wp_enqueue_script('jquery');
-	// wp_enqueue_script('search', get_template_directory_uri() . '/js/search.js');
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('search', get_template_directory_uri() . '/js/search.js');
 	
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
@@ -110,5 +110,8 @@ require get_template_directory() . '/inc/extras.php';
 
 /**load all the scripts */
 // require get_template_directory() . '/inc/enqueue.php';
+// require get_template_directory() . '/inc/sidebar.php';
+
+
 
 
