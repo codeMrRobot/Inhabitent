a7446c8962014b4c731f91d9dc123e69563d28d2
Tony <diablotony01@hotmail.com>
2018-05-21 15:06:06 -0700

    edit about page added image / removed border bottom from nav scss/ removed side bar from page.php

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -9,6 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class=about-banner>
+			</div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -19,5 +21,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
